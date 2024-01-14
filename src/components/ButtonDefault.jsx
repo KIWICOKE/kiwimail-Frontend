d7e335345e1f86adf224a0f362bb11e6ceb52b33
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-15 04:19:48 +0900

    feat: ButtonDefault 제작

---
+++ src/components/ButtonDefault.jsx
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+const ButtonDefault = () => {
+  return (
+    <button
+      css={css`
+        width: 327px;
+        height: 56px;
+        background-color: #d9d9d9;
+        color: #000000;
+      `}
+    ></button>
+  );
+};
+
+export default ButtonDefault;
