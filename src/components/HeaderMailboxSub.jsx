98aadf837265f9391b267f627898a2210215350a
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-15 04:34:17 +0900

    feat: HeaderMailboxSub 형태 구현

---
+++ src/components/HeaderMailboxSub.jsx
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+const HeaderMailboxSub = ({ count }) => {
+  return (
+    <div
+      css={css`
+        width: 327px;
+        height: 24px;
+        background-color: #d9d9d9;
+        padding-left: 32px;
+      `}
+    >
+      {count}개의 편지가 있습니다!
+    </div>
+  );
+};
