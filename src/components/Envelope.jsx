2bcc433e19ffdc227c23ace9680c1c51f824b850
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-23 14:48:11 +0900

    feat: Envelope 컴포넌트 구현

---
+++ src/components/Envelope.jsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+import envelope from "../assets/envelope.webp";
+
+const Envelope = () => {
+  return (
+    <div>
+      <img src={envelope} alt="envelope"></img>
+      <div>이혁</div>
+    </div>
+  );
+};
+
+export default Envelope;
