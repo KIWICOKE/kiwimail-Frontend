326d194edf97e75627b3eb214993861c2c82e000
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-25 03:51:04 +0900

    feat: PostPage 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import ComposePage from "./pages/ComposePage";
+
+import TestPage from "./pages-new/TestPage";
+import SignPage from "./pages-new/SignPage";
+
 import SignupPage from "./pages-new/SignupPage";
 
 import PostboxPage from "./pages-new/PostboxPage";
 import PostListPage from "./pages/PostListPage";
-import SignPage from "./pages-new/SignPage";
 
-import TestPage from "./pages-new/TestPage";
+import PostPage from "./pages-new/PostPage";
+import ComposePage from "./pages/ComposePage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<TestPage />}></Route>
+
         <Route path="/sign" element={<SignPage />}></Route>
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/postbox" element={<PostboxPage />} />
+
+        <Route path="/postbox/:uuid" element={<PostboxPage />} />
+        <Route path="/postlist/:uuid" element={<PostListPage />} />
+
+        <Route path="/post/:pid" element={<PostPage />} />
         <Route path="/compose" element={<ComposePage />} />
-        <Route path="/post" element={<PostListPage />} />
       </Routes>
     </>
   );
---
+++ src/pages-new/PostPage.jsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+
+const PostPage = () => {
+  return <div></div>;
+};
+
+export default PostPage;
