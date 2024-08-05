c9ce0f5b690a3633866c31de17b7a666a7e8d01f
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-08-06 06:04:30 +0900

    feat: 로그인 페이지 수정

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,3 @@
 import styled from "@emotion/styled";
 
-export default Input = styled.input`
-  width: 295px;
-  height: 16px;
-  background-color: #f5f5f5;
-  color: #000000;
-  border-radius: 20px;
-  padding: 12px 16px 12px 16px;
-  font-size: 12px;
-  border: none;
-`;
+export default Input;
--- src/widgets/InputForm.jsx
+++ src/widgets/InputForm.jsx
@@ -1,5 +1,29 @@
 import styled from "@emotion/styled";
+import { Space, Flex } from "../shared/Tool";
+import { useState } from "react";
+
+const Wrapper = styled(Flex)``;
+
+const Input = styled.input`
+  width: 295px;
+  height: 16px;
+  background-color: #f5f5f5;
+  color: #000000;
+  border-radius: 20px;
+  padding: 12px 16px 12px 16px;
+  font-size: 12px;
+  border: none;
+`;
 
 export const InputForm = () => {
-  return;
+  let [name, setName] = useState("");
+  return (
+    <Wrapper direction="column">
+      <form>
+        <Input placeholder={"이름"}></Input>
+        <Input placeholder={"Instagram ID"}></Input>
+        <Input placeholder={"E-mail"}></Input>
+      </form>
+    </Wrapper>
+  );
 };
