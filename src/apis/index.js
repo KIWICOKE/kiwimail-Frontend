1c0bc14ae19bb77af2f1237d69ba3108020afb47
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-08-06 21:02:44 +0900

    chore: 프로젝트 삭제

--- src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { BASE_URL } from "../shared/constant";
-
-const axiosClient = axios.create({
-  baseURL: BASE_URL,
-  Headers: { "Content-Type": "application/json" },
-});
-
-export default axiosClient;
--- src/styles/palette.jsx
+++
@@ -1,4 +0,0 @@
-export const palette = {
-  default_gray: "F5F5F5",
-  default_white: "FFFFFF",
-};
