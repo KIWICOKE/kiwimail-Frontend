2af2b775e2107ff3da8a73936dd665be9a5d0915
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-10-12 01:18:05 +0900

    fix: 사소한 오타 수정

--- src/apis/loginAPI.jsx
+++ src/apis/loginAPI.jsx
@@ -2,11 +2,11 @@ import axiosClient from ".";
 
 const loginAPI = {
   SIGNIN: async () => {
-    const response = await axiosClient.post(`/api/login/`);
+    const response = await axiosClient.post(`/api/login`);
     return response.data; // success, data {token, user_index, name, insta, postCount}
   },
   SIGNUP: async ({ Name, Insta }) => {
-    const response = await axiosClient.post(`/api/signup/`, {
+    const response = await axiosClient.post(`/api/signup`, {
       name: Name,
       insta: Insta,
     });
--- src/shared/constant.jsx
+++ src/shared/constant.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://kiwicoke.com";
+export const BASE_URL = "https://kiwicoke.com:8000";
 // CORS 문제가 발생하는 듯?
